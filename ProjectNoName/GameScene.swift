b5efee56cf32b60b343397d7981bd3a58e3c1b21
j13hernandez <1.hernandez.j.3@gmail.com>
2016-12-19 19:16:21 -0500

    Uploading initial work

--- ProjectNoName/GameScene.swift
+++ ProjectNoName/GameScene.swift
@@ -2,88 +2,66 @@
 //  GameScene.swift
 //  ProjectNoName
 //
-//  Created by Julio Hernandez on 12/15/16.
+//  Created by Julio "Jay Palm" Hernandez on 12/15/16.
 //  Copyright © 2016 Palm Studios. All rights reserved.
 //
 
 import SpriteKit
 import GameplayKit
 
+//Feel free to change varible names to more conventional ones - Jay Palm
+
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+
+    //Global variables
+    var ball: SKShapeNode!
+    var foregroundNode: SKNode!
+
+    //Not sure what this is tbh lol
+    required init?(coder aDecoder: NSCoder) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        super.init(coder: aDecoder)
+    }
+    
+    //Esentially out main() function
+    override init(size: CGSize){
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        super.init(size: size)
+        //Gravity for our universe
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        backgroundColor = SKColor.black
+        //Add Moving Layer
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        
+        //Add Player
+        ball = createPlayer()
+        foregroundNode.addChild(ball)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
+    //Should push our ball up. I think it's not because our ball doesnt have a platform to push off
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+
+        if (ball.physicsBody?.isDynamic)!{
+            return
         }
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20.0))
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    //Player/ball function
+    func createPlayer() -> SKShapeNode {
+        
+        let ball = SKShapeNode(circleOfRadius: 5)
+        ball.strokeColor = SKColor.white
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 5)
+        ball.fillColor = SKColor.white
+        ball.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
+        
+        return ball
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
 }
