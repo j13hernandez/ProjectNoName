39113847fcd40821c92ce6bfe6d78887dcdf7f47
corylennox <corylennox95@gmail.com>
2017-04-12 18:35:36 -0400

    test

--- ProjectNoName/GameViewController.swift
+++ ProjectNoName/GameViewController.swift
@@ -14,6 +14,8 @@ class GameViewController: UIViewController {
 
     
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         let scene = GameScene(size: view.bounds.size)
         let skView = view as! SKView
